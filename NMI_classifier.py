658e02116b9bfd01f8bfe5a4d5b3778d37f8245e
Michael Capps <mindfulmike@gmail.com>
2018-02-08 22:09:44 -0700

    Having some problems with plots, but the model appears to be training correctly.

--- NMI_classifier.py
+++ NMI_classifier.py
@@ -6,6 +6,8 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 
+model_path = '/Users/Mike/Desktop/CNN_test/NavalMineIdentifier_classify/'
+
 def read_dataset(dataset):
 	#read into a dataframe
 	df = pd.read_csv(dataset)
@@ -36,7 +38,7 @@ train_x, test_x, train_y, test_y = train_test_split(X,Y,test_size = 0.2,random_s
 
 # Set learning parameters
 learning_rate = 0.3
-learning_epochs = 1000
+learning_epochs = 100
 cost_history = np.empty(shape = [1],dtype = float)
 n_dim = X.shape[1]
 n_class = 2
@@ -67,6 +69,66 @@ def multilayer_perceptron(x,weights,biases):
 
 	out_layer = tf.add(tf.matmul(layer_4,weights['out']),biases['out'])
 	return out_layer
+
+weights = {
+	'h1': tf.Variable(tf.truncated_normal([n_dim,n_hidden_1])),
+	'h2': tf.Variable(tf.truncated_normal([n_hidden_1,n_hidden_2])),
+	'h3': tf.Variable(tf.truncated_normal([n_hidden_2,n_hidden_3])),
+	'h4': tf.Variable(tf.truncated_normal([n_hidden_3,n_hidden_4])),
+	'out': tf.Variable(tf.truncated_normal([n_hidden_4,n_class]))
+}
+
+biases = {
+	'b1': tf.Variable(tf.truncated_normal([n_hidden_1])),
+	'b2': tf.Variable(tf.truncated_normal([n_hidden_2])),
+	'b3': tf.Variable(tf.truncated_normal([n_hidden_3])),
+	'b4': tf.Variable(tf.truncated_normal([n_hidden_4])),
+	'out': tf.Variable(tf.truncated_normal([n_class]))
+}
+
+init = tf.global_variables_initializer()
+
+saver = tf.train.Saver()
+
+y = multilayer_perceptron(x,weights,biases)
+
+cost_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = y, labels = y_))
+training_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_function)
+
+sess = tf.Session()
+sess.run(init)
+
+mse_history = []
+accuracy_history = []
+
+for epoch in range(learning_epochs):
+	sess.run(training_step,feed_dict={x:train_x,y_:train_y})
+	cost = sess.run(cost_function,feed_dict={x:train_x,y_:train_y})
+	cost_history = np.append(cost_history,cost)
+	correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+	accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+	print('Accuracy:',sess.run(accuracy,feed_dict={x:train_x,y_:train_y}))
+	pred_y = sess.run(y,feed_dict = {x:test_x})
+	mse = tf.reduce_mean(tf.square(pred_y-test_y))
+	mse_ = sess.run(mse)
+	mse_history.append(mse_)
+	accuracy = sess.run(accuracy,feed_dict={x:train_x,y_:train_y})
+	accuracy_history.append(accuracy)
+
+	print("Epoch: ",epoch,' - cost: ',cost, ' - MSE: ',mse_,' - Train accuracy: ',accuracy)
+
+save_path = saver.save(sess,model_path)
+print("Model saved in file %s:" % save_path)
+
+
+plt.plot(mse_history,'r')
+plt.show()
+plt.plot(accuracy_history)
+plt.show()
+
+
+
+
 
 
 
